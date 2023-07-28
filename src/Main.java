b40d6ef2d4637b5b20cb0c25c6ec05d1874ba379
Sky Shah <Dylan,shah@outlook.com>
2023-07-28 12:29:32 -0700

    U1L6A1

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Please enter a four digit number: ");
-        int number = scanner.nextInt();
-        if (number > 9999 | number < 1000) throw new Exception("Invalid Input");
-        int[] numbers = new int[4];
-        numbers[0] = number / 1000;
-        number %= 1000;
-        numbers[1] = number / 100;
-        number %= 100;
-        numbers[2] = number / 10;
-        number %= 10;
-        numbers[3] = number;
-        System.out.printf("Here are the digits:\n%d\n%d\n%d\n%d", numbers[3], numbers[2], numbers[1], numbers[0]);
-
+        
+        System.out.print("Please enter the numerator: ");
+        double Numerator = scanner.nextDouble();
+        System.out.print("Please enter the denominator: ");
+        double Denominator = scanner.nextDouble();
+        System.out.printf("The decimal value is: %.3f", Numerator/Denominator);
     }
 }
