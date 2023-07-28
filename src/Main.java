cbbd8576b2c4eeadc7926cee49890633a705702e
Sky Shah <Dylan,shah@outlook.com>
2023-07-28 11:56:21 -0700

    U1L5A2

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,22 @@ import java.util.Scanner;  // Import the Scanner class
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("How many feet do you have? ");
-        double feet = scanner.nextDouble();
-        System.out.print((int) feet / 3);
+        System.out.print("Please enter a four digit number: ");
+        int number = scanner.nextInt();
+        if (number > 9999 | number < 1000) throw new Exception("Invalid Input");
+        int[] numbers = new int[4];
+        numbers[0] = number / 1000;
+        number %= 1000;
+        numbers[1] = number / 100;
+        number %= 100;
+        numbers[2] = number / 10;
+        number %= 10;
+        numbers[3] = number;
+        System.out.printf("Here are the digits:\n%d\n%d\n%d\n%d", numbers[3], numbers[2], numbers[1], numbers[0]);
 
     }
 }
