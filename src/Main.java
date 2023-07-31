1c89f74aed04602644319bad4abf9fd46a2ff6cb
Sky Shah <Dylan,shah@outlook.com>
2023-07-31 11:47:42 -0700

    U1L6A3

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,23 @@ import java.util.Scanner;  // Import the Scanner class
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+  public static void main( String [] args) throws Exception {
 
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Please enter the numerator: ");
-        double Numerator = scanner.nextDouble();
-        System.out.print("Please enter the denominator: ");
-        double Denominator = scanner.nextDouble();
-        System.out.printf("The decimal value is: %.3f", Numerator/Denominator);
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Please input a decimal number:");
+    double number = scanner.nextDouble();
+    number -= (int) number;
+    int[] digits = new int[3];
+    for (int i = 0; i < 3; i++) {
+      number *= 10;
+      digits[i] = (int) number;
+      number -= (int) number;
     }
+    System.out.printf("Answer: %d %d %d", digits[0], digits[1], digits[2]);
+
+
+
+    if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
+  }
 }
