62d70cea0c66950561b58c928d22197fe0af6f24
Sky Shah <Dylan,shah@outlook.com>
2023-08-08 12:22:06 -0700

    U2L1A1

--- src/Main.java
+++ src/Main.java
@@ -5,52 +5,12 @@ public class Main {
   
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
-
-//    Course Name
-    System.out.print("Please enter the course name: ");
-    String courseName = scanner.nextLine();
-
-//    Time (min)
-    System.out.print("Please enter the average time spent in a week for this course in minutes: ");
-    int minutes = scanner.nextInt();
-    String time = minutes / 60 + "h" + minutes % 60;
-
-//    Homework
-    System.out.print("Please enter your homework scores:\n");
-    int[] homework = {scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt()};
-
-//    Quizzes
-    System.out.print("Please enter your quiz scores:\n");
-    double[] quizzes = {scanner.nextDouble(), scanner.nextDouble()};
-
-//    Final
-    System.out.print("Please enter your final exam score:\n");
-    double finalExam = scanner.nextDouble();
     
-    long overallGrade = Math.round(
-        /*     35% weight for Homework */ 0.35 * Arrays.stream(homework).average().orElse(Double.NaN) +
-            /* 15% weight for Quizzes  */ 0.15 * Arrays.stream(quizzes).average().orElse(Double.NaN) +
-            /* 35% weight for Final Exm*/ 0.50 * finalExam
-    
-    );
-    
-//    Print
-    System.out.printf(
-        """
-        Course name: %s
-        Weekly time spent: %s
-        Average homework grade: %.2f
-        Average quiz grade: %.2f
-        Final exam grade: %.2f
-        Overall grade: %d
-        """,
-        courseName,
-        time,
-        (float) Arrays.stream(homework).average().orElse(Double.NaN),
-        (float) Arrays.stream(quizzes).average().orElse(Double.NaN),
-        finalExam,
-        overallGrade
-    );
+    System.out.println("What is your name?");
+    String name = scanner.nextLine();
+    System.out.println("What is your favorite number?");
+    int number = scanner.nextInt();
+    System.out.printf("Your name is %s and you like the number %d.", name, number);
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
