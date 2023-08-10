a6c0d615409be848291737ba27dcdada30c06e62
Sky Shah <Dylan,shah@outlook.com>
2023-08-10 09:55:24 -0700

    U2L2A2

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,7 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("What type of item are you buying?");
-    String item = scanner.nextLine();
-    System.out.println("How many are you buying?");
-    int quantity = scanner.nextInt();
-    System.out.println("How much does each one weigh?");
-    float weight = scanner.nextFloat();
-    System.out.printf("%d %s at %.2f pounds each will weigh %.2f pounds total", quantity, item, weight, quantity * weight);
+    System.out.print("\"That brain of mine is something more than merely mortal; as time will show.\"\nAda Lovelace\nThe first computer programmer");
     
     
     
