59c203015c677b06434207e2f24151bba482d9be
Sky Shah <Dylan,shah@outlook.com>
2023-08-10 10:19:38 -0700

    U2L3A1

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,12 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("(\\(\\\n( - -)\n((') (')");
+    System.out.println("Enter a string:");
+    String word = scanner.next();
+    System.out.println("Enter a number:");
+    int index = scanner.nextInt();
+    
+    System.out.printf("%s%s", word.substring(0, index), word.substring(word.length() - index));
     
     
     
