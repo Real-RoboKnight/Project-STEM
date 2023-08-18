380a5e3e5a146eb27b242700bf5df4f1cf8704ce
Sky Shah <Dylan,shah@outlook.com>
2023-08-18 11:40:18 -0700

    U2L8A2

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,16 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.print("Enter a positive integer: ");
-    int n = scanner.nextInt() + 1;
-    System.out.println((int) (n * Math.random()) + 2);
-    System.out.println((int) (n * Math.random()) + 2);
-    System.out.println((int) (n * Math.random()) + 2);
+    System.out.print("Enter X1: ");
+    double X1 = scanner.nextDouble();
+    System.out.print("Enter X2: ");
+    double X2 = scanner.nextDouble();
+    System.out.print("Enter Y1: ");
+    double Y1 = scanner.nextDouble();
+    System.out.print("Enter Y2: ");
+    double Y2 = scanner.nextDouble();
+    System.out.print("Slope: " + (Y2 - Y1) / (X2 - X1));
+    
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
