4d995994f2fd05246894bdc116c957d66db2d851
Sky Shah <Dylan,shah@outlook.com>
2023-08-18 11:36:43 -0700

    U2L8A1

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;  // Import the Scanner class
-import shapes.*
+
 public class Main {
   
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-  
-    String str1 = scanner.nextLine();
-    String str2 = new String(str1);
-    str1 = str1.toUpperCase();
-    str2 = str2.substring(0,1).toUpperCase() + str2.substring(1);
-    System.out.println(str2);
-    System.out.println(str1);
-    
+    System.out.print("Enter a positive integer: ");
+    int n = scanner.nextInt() + 1;
+    System.out.println((int) (n * Math.random()) + 2);
+    System.out.println((int) (n * Math.random()) + 2);
+    System.out.println((int) (n * Math.random()) + 2);
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
