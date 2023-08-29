39c0ee246e6b4e7b21e3a645a5a52e7b249a0a04
Sky Shah <Dylan,shah@outlook.com>
2023-08-29 12:01:28 -0700

    U3L1A4

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("Please enter a integer:");
-    if (scanner.nextInt() == 48) System.out.println("YES");
+    System.out.println("Please enter an integer:");
+    int num = scanner.nextInt();
+    
+    if (num % 2 == 0) System.out.println("Divisible by 2!");
+    if (num % 3 == 0) System.out.println("Divisible by 3!");
+    
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
 }
