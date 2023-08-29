bff71de716d9f7ce478385d39c6c4f2a94bed521
Sky Shah <Dylan,shah@outlook.com>
2023-08-29 11:46:16 -0700

    U3L1A2

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("Please enter a double:");
-    if (scanner.nextDouble() == 12.345) System.out.println("YES");
+    System.out.println("Please enter a integer:");
+    if (scanner.nextInt() == 48) System.out.println("YES");
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
 }
