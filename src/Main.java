4b877d5cca7dde8aca8c9c82ae367e35cf239f85
Sky Shah <Dylan,shah@outlook.com>
2023-08-31 11:43:08 -0700

    U3L3A2

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,21 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("Please enter an integer");
-    System.out.println( (scanner.nextInt() % 2 == 0) ? "Even" : "Odd" );
-    if (false) {} else {}
+    System.out.println("Please enter a letter grade:");
+    String next = scanner.next();
+    if (next.equals("A")) {
+      System.out.println("90-100");
+    } else if (next.equals("B")) {
+      System.out.println("80-90");
+    } else if (next.equals("C")) {
+      System.out.println("70-80");
+    } else if (next.equals("D")) {
+      System.out.println("60-70");
+    } else if (next.equals("F")) {
+      System.out.println("0-60");
+    } else {
+      System.out.println("Invalid letter grade");
+    }
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
