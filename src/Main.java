d2ff0e14c0d6942ac35377cf60d4aba94a601169
Sky Shah <Dylan,shah@outlook.com>
2023-08-31 11:54:24 -0700

    U3L3A3

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,13 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("Please enter a letter grade:");
-    String next = scanner.next();
-    if (next.equals("A")) {
-      System.out.println("90-100");
-    } else if (next.equals("B")) {
-      System.out.println("80-90");
-    } else if (next.equals("C")) {
-      System.out.println("70-80");
-    } else if (next.equals("D")) {
-      System.out.println("60-70");
-    } else if (next.equals("F")) {
-      System.out.println("0-60");
-    } else {
-      System.out.println("Invalid letter grade");
-    }
+    System.out.println("Please enter two integers:");
+    int one = scanner.nextInt();
+    int two = scanner.nextInt();
+    System.out.printf("%d + %d = ?\n", one, two);
+    if (scanner.nextInt() == one + two) System.out.println("Correct!");
+    else System.out.println("Wrong");
+    
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
