21dd22ec8e943283a6062d3f88145d34929bd6a8
Sky Shah <Dylan,shah@outlook.com>
2023-09-05 11:59:47 -0700

    U3L4A3

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,15 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("Enter two number:");
-    int[] input = {scanner.nextInt(), scanner.nextInt()};
-    System.out.println( (input[0] >= 0 && input[1] >= 0 && input[0] % 2 == 0 && input[1] % 2 == 0) ? "Both are positive and even." : "At least one is negative or odd.");
+    System.out.print("Please enter the latitude: ");
+    double latitude = scanner.nextDouble();
+    System.out.print("Please enter the longitude: ");
+    double longitude = scanner.nextDouble();
+    
+    System.out.print( (-90 <= latitude && latitude <= 90) ? "" : "latitude is incorrect\n" );
+    System.out.print( (-180 <= longitude && longitude <= 180) ? "" : "longitude is incorrect\n" );
+    System.out.println( (-90 <= latitude && latitude <= 90) && (-180 <= longitude && longitude <= 180) ?
+        "The location: " + latitude + ", " + longitude : "");
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
