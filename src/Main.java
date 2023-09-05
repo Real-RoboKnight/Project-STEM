fac22ca8c3cc7400bb1946e56247cb43045b90a1
Sky Shah <Dylan,shah@outlook.com>
2023-09-05 11:50:22 -0700

    U3L4A2

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("Enter a number:");
-    int input = scanner.nextInt();
-    System.out.println( (65 <= input && input <= 100) ? "False" : "True" );
+    System.out.println("Enter two number:");
+    int[] input = {scanner.nextInt(), scanner.nextInt()};
+    System.out.println( (input[0] >= 0 && input[1] >= 0 && input[0] % 2 == 0 && input[1] % 2 == 0) ? "Both are positive and even." : "At least one is negative or odd.");
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
