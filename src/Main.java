469ff30c67b416acbaa4a5697ec3fcf41355bf2a
Sky Shah <Dylan,shah@outlook.com>
2023-09-07 11:17:12 -0700

    U3L7A1

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,8 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Enter two numbers:");
-    int a = scan.nextInt();
-    int b = scan.nextInt();
-    
-    if(( a != 0 && b == 0 || a % b != 0))
-      System.out.println(b + " is not a factor of " + a);
-    else
-      System.out.println(b + " is a factor of " + a);
+    System.out.println("Enter password:");
+    System.out.println( (scanner.next().equals("bulbasaur")) ? "Access granted!" : "Access denied!");
   
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
