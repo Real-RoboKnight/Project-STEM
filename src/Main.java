36572e47e47200899a18d4f27d2e30cb1d2ba587
Sky Shah <Dylan,shah@outlook.com>
2023-09-08 11:53:17 -0700

    U4L1A2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
+import java.util.Collections;
 import java.util.Scanner;  // Import the Scanner class
+import java.util.ArrayList; // import the ArrayList class
 
 public class Main {
   
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("Enter any numbers (Enter 5 to stop)");
-    double sum = 0;
-    double input;
-    while ((input = scanner.nextDouble()) != 5)
-      sum += input;
-    System.out.println("Sum is " + sum);
-    
+    System.out.println("Enter the Scores:");
+     ArrayList<Integer> scores = new ArrayList<Integer>();
+     int currentScores;
+     while ((currentScores = scanner.nextInt()) != -1)
+       scores.add(currentScores);
+    System.out.println("The largest score is " + Collections.max(scores));
+
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
 }
