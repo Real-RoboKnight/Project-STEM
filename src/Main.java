8edd409b503c1d1fd22828342761490bd86d87ee
Sky Shah <Dylan,shah@outlook.com>
2023-09-08 11:35:40 -0700

    U4L1A1

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,12 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("Welcome. What is your name?");
-    System.out.printf("Hello %s. Are you ready to crack the code?\n", scanner.nextLine());
-    if (!scanner.nextLine().equalsIgnoreCase("Yes"))
-      return;
-    
-    System.out.println("\nPhase 1\nEnter a string:");
-    if (! (scanner.nextLine().length() == 3)) {
-      System.out.println("Sorry, that was incorrect!\n" + "Better luck next time!");
-      return;
-    }
-    
-    System.out.println("Correct!\n\nPHASE 2\nEnter a number:");
-    int num = scanner.nextInt();
-    if (!( num == 19 || (num >= 35 && num < 78))) {
-      System.out.println("Sorry, that was incorrect!\n" + "Better luck next time!");
-      return;
-    }
-    
-    System.out.println("Correct!\n\nPHASE 3\nEnter a number:");
-    int num2 = scanner.nextInt();
-    if (!(num2 > 0 && (num2 % 2 == 0 || num2 % 10 == 1) )) {
-      System.out.println("Sorry, that was incorrect!\n" + "Better luck next time!");
-      return;
-    }
-    System.out.println("Correct!\nYou have cracked the code!");
+    System.out.println("Enter any numbers (Enter 5 to stop)");
+    double sum = 0;
+    double input;
+    while ((input = scanner.nextDouble()) != 5)
+      sum += input;
+    System.out.println("Sum is " + sum);
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
