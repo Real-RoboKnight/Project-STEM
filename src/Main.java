f3e6ed9c376c3e3c162ddc0eb3db006c13fa9399
Sky Shah <Dylan,shah@outlook.com>
2023-09-11 11:28:44 -0700

    U4L1.5A1

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,11 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("Input a word:");
-    String word = scanner.nextLine();
-    
-    int current_index = 0;
-    while (word.length() > current_index + 3) {
-      System.out.print(word.substring(current_index, current_index + 2));
-      current_index += 3;
+    Scanner scan = new Scanner(System.in);
+    int num = scan.nextInt();
+    for (int i = 1; i <= 5; i++) {
+      System.out.println(num*i);
     }
-    System.out.println(word.substring(current_index));
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
