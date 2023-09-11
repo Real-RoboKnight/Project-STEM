adb3a5a7b20509a194201271aca4b3bf0b5557ae
Sky Shah <Dylan,shah@outlook.com>
2023-09-11 12:09:36 -0700

    U4L2A1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;  // Import the Scanner class
-import java.util.stream.IntStream;
 
 public class Main {
   
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.print("N: ");
-    int n = scanner.nextInt();
-    System.out.println("Sum = " + IntStream.range(1, n+1).sum());
+    System.out.print("Enter two numbers:\n");
+    int num1 = scanner.nextInt();
+    num1 += (num1 % 2 == 1) ? 0 : 1;
+    
+    int num2 = scanner.nextInt();
+    num2 += (num2 % 2 == 1) ? 0 : -1;
+    
+    System.out.println();
+    for (int i = num1; i <= num2; i+=2) {
+      System.out.print(i + " ");
+    }
+    
+    
     
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
