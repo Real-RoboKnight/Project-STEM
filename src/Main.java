0cc47bed6c1711cb99f089f0e1fb25c0158cfb7b
Sky Shah <Dylan,shah@outlook.com>
2023-09-11 11:39:08 -0700

    U4L1.5A2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;  // Import the Scanner class
+import java.util.stream.IntStream;
 
 public class Main {
   
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    Scanner scan = new Scanner(System.in);
-    int num = scan.nextInt();
-    for (int i = 1; i <= 5; i++) {
-      System.out.println(num*i);
-    }
+    System.out.print("N: ");
+    int n = scanner.nextInt();
+    System.out.println("Sum = " + IntStream.range(1, n+1).sum());
+    
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
   }
