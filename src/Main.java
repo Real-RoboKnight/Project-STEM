21342b67cc3e36b362c16e9d963cbcd8278afd23
Sky Shah <Dylan,shah@outlook.com>
2023-09-12 12:19:39 -0700

    U4L3A2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;  // Import the Scanner class
 import java.util.stream.IntStream;
 
@@ -8,10 +6,9 @@ public class Main {
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-//    IntStream.range(0, 13).map(value -> value * 2 + 1).forEach(System.out::println);
-    
-    for (int x : IntStream.range(0, 13).toArray()) {
-      System.out.println(x*2+1);
+    for (int i = 17; i <= 73; i += 10) {
+      IntStream.range(i, i + 10 >= 73 ? 74 : i + 10).forEach(value -> System.out.print(value + " "));
+      System.out.println();
     }
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
