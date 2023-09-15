11fa92ebcc83df67d0bce685c578095356856b00
Sky Shah <Dylan,shah@outlook.com>
2023-09-15 12:09:01 -0700

    U4L5A2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;  // Import the Scanner class
 import java.util.stream.IntStream;
+//import java.util.stream.IntStream;
 
 public class Main {
   
   public static void main (String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("Input a String:");
-    char[] input = scanner.nextLine().toCharArray();
-    System.out.println("Input an integer:");
-    int repeat = scanner.nextInt();
-    
-    for (char letter : input) {
-      for (int x = 0; x <= repeat; x++) {
-        System.out.print(letter);
+    for (int i = 10; i > 0 ; i--) {
+      for (int x = 0; x < i; x++) {
+        System.out.print(i + " ");
       }
+      System.out.println();
     }
     
     if (args.length == Integer.MAX_VALUE) throw new Exception("EndDocument");
