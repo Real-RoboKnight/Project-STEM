d06df4f90a8c54cf9530b72f141eac0f929b2d73
Dylan Shah <dylan.shah@outlook.com>
2023-10-20 12:09:02 -0700

    U4A4 Algo 1 finished

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
   public static void main (String[] args) {
     Scanner scanner = new Scanner(System.in);
@@ -11,17 +12,25 @@ public class Main {
 
 //    Algo 1:
     List<Object> AlgoOneOutput = algoOne(rawInput);
-    System.out.printf("Vowels removed: %d\nRepeats removed: %d\nAlgorithm 1 message: %s\nAlgorithm 1 characters saved: %d", (int)AlgoOneOutput.get(0), (int)AlgoOneOutput.get(1), AlgoOneOutput.get(2), (int)AlgoOneOutput.get(3));
-    
+    System.out.printf("\nAlgorithm 1\nVowels removed: %d\nRepeats removed: %d\nAlgorithm 1 message: %s\nAlgorithm 1 characters saved: %d\n", (int)AlgoOneOutput.get(0), (int)AlgoOneOutput.get(1), AlgoOneOutput.get(2), (int)AlgoOneOutput.get(3));
+
+//    Algo 2:
+    List<Object> AlgoTwoOutput = algoTwo(rawInput);
+    System.out.printf("\nAlgorithm 2\nUnique characters found: %d\nAlgorithm 2 message: %s\nAlgorithm 2 characters saved: %d", (int)AlgoTwoOutput.get(0), AlgoTwoOutput.get(1), (int)AlgoTwoOutput.get(2));
   }
+  
+  /**
+   * This should be an ascii character that is not used in any input, that the vowels can temporarily be, in order for the repeat character remover to not go crazy.
+   */
   static Character vowelPlaceholder = '`';
+  
   private static List<Object> algoOne (String input) {
     
     List<Object> vowelsRemoved = RemovesVowels(input);
     List<Object> repeatsRemoved = RemovesRepeats((String)vowelsRemoved.get(1));
     int vowelCount = (int)vowelsRemoved.get(0);
     int repeatsCount = (int)repeatsRemoved.get(0);
-    String output = RemovesAllOccurrences((String) repeatsRemoved.get(1), vowelPlaceholder);
+    String output = RemovesAllOccurrences((String)repeatsRemoved.get(1), vowelPlaceholder);
     return Arrays.asList(vowelCount, repeatsCount, output, vowelCount + repeatsCount);
   }
   
@@ -52,6 +61,7 @@ public class Main {
 //    Add the space back after words end
       vowelsRemovedStr.append(' ');
     }
+    
     return Arrays.asList(vowelCount, vowelsRemovedStr.toString());
   }
   
@@ -68,5 +78,23 @@ public class Main {
   
   private static String RemovesAllOccurrences (String input, Character Character) {
     return input.replace(Character.toString(), "");
+  }
+  
+  private static long NumOfOccurrences (String input, Character Character) {
+    return input.chars().filter(c -> c == Character).count();
+  }
+  
+  private static List<Object> algoTwo (String input) {
+    int charactersSaved = input.length();
+    StringBuilder message = new StringBuilder();
+    input = RemovesAllOccurrences(input, ' ');
+    
+    while (input.length() != 0) {
+      long numberOfCharacter = NumOfOccurrences(input, input.charAt(0));
+      message.append(numberOfCharacter).append(input.charAt(0));
+      input = RemovesAllOccurrences(input, input.charAt(0));
+    }
+    
+    return Arrays.asList(message.length()/2, message.toString(), charactersSaved - message.length());
   }
 }
