4db046b67c9438c4f18dfc620b8671f32f27bd06
Dylan Shah <dylan.shah@outlook.com>
2023-10-30 12:03:12 -0700

    U5L1A4

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,18 @@ public class Main {
   
   public static void main (String[] args) {
     Scanner scanner = new Scanner(System.in);
-    printDouble(scanner.nextDouble(), scanner.nextInt());
+    coinConverter(scanner.nextInt());
   }
   
-  public static void printDouble(double num, int times) {
-    IntStream.range(0, times).parallel().forEach(n -> System.out.println(num));
+  public static void coinConverter (int cents) {
+    System.out.printf("Dollar Bills: %d\n", cents / 100);
+    cents %= 100;
+    System.out.printf("Quarters: %d\n", cents / 25);
+    cents %= 25;
+    System.out.printf("Dimes: %d\n", cents / 10);
+    cents %= 10;
+    System.out.printf("Nickels: %d\n", cents / 5);
+    cents %= 5;
+    System.out.printf("Pennies: %d\n", cents);
   }
 }
